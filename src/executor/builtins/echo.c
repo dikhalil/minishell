d60b1d409c19b5fba726347d70d9aad55a177a45
Ahmad Al-khasawneh <ahmad@example.com>
2025-11-08 19:10:19 +0300

    fixed it to handle a lot of n not just one

--- src/executor/builtins/echo.c
+++ src/executor/builtins/echo.c
@@ -6,11 +6,27 @@
 /*   By: yocto <yocto@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 17:21:38 by yocto             #+#    #+#             */
-/*   Updated: 2025/11/03 17:21:39 by yocto            ###   ########.fr       */
+/*   Updated: 2025/11/08 19:08:40 by yocto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	is_n_flag(char *s)
+{
+	int	i;
+
+	if (!s || s[0] != '-' || s[1] != 'n')
+		return (0);
+	i = 2;
+	while (s[i])
+	{
+		if (s[i] != 'n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 void	echo_builtin(t_data *data, t_arg *args)
 {
@@ -19,7 +35,7 @@ void	echo_builtin(t_data *data, t_arg *args)
 
 	newline = 1;
 	str_exit = ft_itoa(data->last_exit);
-	if (args && ft_strcmp(args->value, "-n") == 0)
+	while (args && is_n_flag(args->value))
 	{
 		newline = 0;
 		args = args->next;
